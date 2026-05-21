2266884acd203f5804956a8e8d414dba9af99839
Dipen Thapa <idipen10@gmail.com>
2026-05-20 23:07:17 -0400

    created react app

---
+++ eyesee2/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+import './App.css'
+
+const Welcome = () => {
+  return <h2> welcome dipen</h2>
+}
+
+function Button() {
+  return <button>Click Me</button>
+
+}
+function App() {
+
+  return (
+    <div>
+      hi
+      <Welcome />
+      <Button />
+    </div>
+  )
+}
+
+export default App
