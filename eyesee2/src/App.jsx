4e1377abf968292ca55389f09560e9f7e25421fa
Dipen Thapa <idipen10@gmail.com>
2026-05-24 10:19:34 -0400

    changes made

--- eyesee2/src/App.jsx
+++ eyesee2/src/App.jsx
@@ -1,21 +1,13 @@
-// import { CreateRoom } from "./components/CreateRoom";
-import { JoinRoom } from "./components/JoinRoom";
+
 import { HomeScreen } from "./components/HomeScreen";
-import { GameScreen } from "./components/GameScreen";
-import { useState } from "react";
-import { Counter } from "./components/Counter"
-
 import './App.css'
-
-
-
 
 function App() {
 
   return (
-    <div>
-      {/* <HomeScreen /> */}
-      <Counter />
+    <div id='hero-page'>
+      <HomeScreen />
+
     </div>
   )
 }
