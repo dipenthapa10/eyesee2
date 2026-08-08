b9abef1a85517110498233db45cd09c213234882
Dipen Thapa <idipen10@gmail.com>
2026-08-08 00:34:06 -0400

    serverurl

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3001', {
+const serverUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001'
+
+const socket = io(serverUrl, {
     autoConnect: true,
     reconnection: true,
     reconnectionAttempts: 5,
