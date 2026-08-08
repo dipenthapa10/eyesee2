b3879d836a95980f1533561bbfa4b3695adb6e7a
Dipen Thapa <idipen10@gmail.com>
2026-08-08 00:49:12 -0400

    cors edited

--- server/server.js
+++ server/server.js
@@ -16,9 +16,7 @@ const corsOptions = {
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
-    cors: {
-        cors: corsOptions
-    }
+    cors: corsOptions
 })
 
 
